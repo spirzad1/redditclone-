3c788d8a4266230d061d21ce4c46fd3f92c945f5
Mihai Andrei <mtandrei@gmail.com>
2016-03-25 21:31:34 -0400

    updated models

--- reddit/models.py
+++ reddit/models.py
@@ -1,9 +1,54 @@
 from reddit import app, db
 
-
+#TODO cmeheck ForeignKey syntax
 class UserDB(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     username = db.Column(db.String(24), nullable=False)
     email = db.Column(db.String(48), nullable=False)
     pass_hash = db.Column(db.String(128), nullable=False)
+
+class PostDB(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    title = db.Column(db.String(50), nullable=False)
+    context = db.Column(db.String(1000), nullable=False)
+    author = db.Column(db.ForeignKey("users.username"), nullable=False)
+    num_likes = db.Column(db.Integer, nullable=False)
+    time = db.Column(db.Time, nullable=False)
+
+class CommentDB(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    content = db.Column(db.String(1000), nullable=False)
+    author = db.Column(db.ForeignKey("users.username"), nullable=False)
+    post_id = db.Column(db.ForeignKey("posts.id"), nullable=False)
+    num_likes = db.Column(db.Integer, nullable=False)
+    time = db.Column(db.Time, nullable=False)
+
+class LikeDB(db.Model):
+    __tablename__ = 'likes'
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    username = db.Column(db.ForeignKey("users.id"), nullable=False)
+    postorcomment = db.Column(db.Integer, nullable=False) #TODO see if there's a boolean type
+    comment_id = db.Column(db.ForeignKey("comments.id"), nullable=False)
+    post_id = db.Column(db.ForeignKey("posts.id"), nullable=False)
+    type_like = db.Column(db.String(1), nullable=False)
+
+class TagDB(db.Model):
+    __tablename__ = 'tags'
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String(30), nullable=False)
+
+class PostTagDB(db.Model):
+    __tablename__ = 'posts_tags' 
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    tag_id = db.Column(db.ForeignKey("tags.id"), nullable=False)
+    post_id = db.Column(db.ForeignKey("posts.id"), nullable=False)
+
+class UserTagDB(db.Model):
+    __tablename__ = 'users_tags'
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    tag_id = db.Column(db.ForeignKey("tags.id"), nullable=False)
+    username = db.Column(db.ForeignKey("users.username"), nullable=False)
+    weight = db.Column(db.Integer, nullable=False
