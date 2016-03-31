1bf393a6377acabcb6640101f4c9b7697537130b
Mihai <mtandrei@gmail.com>
2016-03-30 22:17:48 -0400

    db fixed

--- reddit/models.py
+++ reddit/models.py
@@ -3,7 +3,7 @@ from reddit import app, db
 class UserDB(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    username = db.Column(db.String(24), primary_key=True,nullable=False)
+    username = db.Column(db.String(24), unique=True, nullable=False)
     email = db.Column(db.String(48), nullable=False)
     pass_hash = db.Column(db.String(128), nullable=False)
 
