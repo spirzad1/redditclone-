6f51bb72884e63ecba6f7b2ac970b744d0453eac
Sakib Jalal <sakib.jalal@gmail.com>
2016-04-07 05:11:31 -0400

    user implementation

--- reddit/__init__.py
+++ reddit/__init__.py
@@ -3,6 +3,8 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
+from flask.ext.login import LoginManager
+
 
 app = Flask(__name__)
 app.config.from_pyfile('../config.py')
@@ -12,5 +14,8 @@ migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+lm = LoginManager(app)
+lm.login_view = 'home'
 
 from reddit import views, models
--- reddit/models.py
+++ reddit/models.py
@@ -1,10 +1,11 @@
 from reddit import app, db
+from flask.ext.login import UserMixin
 
-class UserDB(db.Model):
+class UserDB(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
+    email = db.Column(db.String(48),unique=True, nullable=False)
     username = db.Column(db.String(24), unique=True, nullable=False)
-    email = db.Column(db.String(48), nullable=False)
     pass_hash = db.Column(db.String(128), nullable=False)
 
 class PostDB(db.Model):
